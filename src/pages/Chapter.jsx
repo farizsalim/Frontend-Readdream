a3b4c209bb6c6c7ed9a006abc6d9828541d933d7
farizsalim <farizsalim10@yahoo.co.id>
2023-11-17 21:15:21 +0800

    DISQUSS REPAIR

--- src/pages/Chapter.jsx
+++ src/pages/Chapter.jsx
@@ -105,7 +105,7 @@ const Chapter = () => {
             {
                (function() { // DON'T EDIT BELOW THIS LINE
                 var d = document, s = d.createElement('script');
-                s.src = {Disquss};
+                s.src = Disquss;
                 s.setAttribute('data-timestamp', +new Date());
                 (d.head || d.body).appendChild(s);
                 })()
